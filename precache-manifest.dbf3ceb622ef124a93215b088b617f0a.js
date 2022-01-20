36c40119fb2588ce4e730a1c04c148ebd15a15eb
amrit <amritpalsoftware@gmail.com>
2022-01-20 16:29:17 +0530

    initial

---
+++ precache-manifest.dbf3ceb622ef124a93215b088b617f0a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9557c502ba072607e8eccd41299bf168",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dc051b9b020a8ac9148",
+    "url": "/static/css/2.7a908399.chunk.css"
+  },
+  {
+    "revision": "ad48939bd829dcd5623a",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "8dc051b9b020a8ac9148",
+    "url": "/static/js/2.0319c1f3.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.0319c1f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad48939bd829dcd5623a",
+    "url": "/static/js/main.711b3325.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "26486665fb0d39f59a63b6be02770722",
+    "url": "/static/media/amrit-pal-resume.26486665.pdf"
+  },
+  {
+    "revision": "6a5d0b9e5b4a46df7de213a5a0367b21",
+    "url": "/static/media/favicon.6a5d0b9e.ico"
+  }
+]);
